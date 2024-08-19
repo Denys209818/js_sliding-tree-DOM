e43988b6293f735aa442b4fee65fe0e8fea5b086
Denys209818 <deniskravcuk63@gmail.com>
2024-08-20 01:21:49 +0300

    make build

---
+++ index.9bab40c3.js
@@ -0,0 +1,2 @@
+document.getElementsByClassName("tree")[0].addEventListener("click",function(e){var t=e.target.closest("span");if(t){var n=t.closest("li").querySelector("ul");n&&(n.hidden=!n.hidden)}});
+//# sourceMappingURL=index.9bab40c3.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+// write code here
+const list = document.getElementsByClassName("tree")[0];
+list.addEventListener("click", (e)=>{
+    const span = e.target.closest("span");
+    if (span) {
+        const current = span.closest("li");
+        const subList = current.querySelector("ul");
+        if (subList) subList.hidden = !subList.hidden;
+    }
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
